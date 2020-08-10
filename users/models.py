04aafc8c93675f179e3ef26fed71ca75402761e4
imrangar304 <shubhamrangar786@gmail.com>
2020-08-10 09:46:09 +0530

    Email and Password Reset

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
@@ -12,9 +12,9 @@ class Profile(models.Model):
     def save(self):
         super().save()
 
-        img =Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
-        if img.height >300 or img.width > 300:
-            output_size = (300,300)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
